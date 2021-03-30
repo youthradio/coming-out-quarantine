002126b5e74ba1f051f147df4d86272a5c057b95
radames <radames@users.noreply.github.com>
2021-03-30 17:19:03 +0000

    deploy: b2d55348d27092c1b6c2d6aae73a4230a353ddb2

---
+++ _nuxt/static/1617124742/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcoming-out-during-quarantine\u002F_nuxt\u002Fstatic\u002F1617124742",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Fcoming-out-during-quarantine\u002F",assetsPath:"\u002Fcoming-out-during-quarantine\u002F_nuxt\u002F",cdnURL:a}}}}(null));
